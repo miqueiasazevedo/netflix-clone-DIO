258e27330cab0e70ed21fc2bd974fa215a8774fe
Miquéias Azevedo <miqueias.azevedo@hotmail.com>
2021-11-06 04:47:04 -0300

    Adicionados arquivos javascript utilizado durante o desenvolvimento

---
+++ js/owl/main.js
@@ -0,0 +1,26 @@
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: false,
+  dots: true,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    400: {
+      items: 2,
+    },
+    700: {
+      items: 3,
+    },
+    1000: {
+      items: 4,
+    },
+    1200: {
+      items: 5,
+    },
+    1500: {
+      items: 6,
+    },
+  },
+});
